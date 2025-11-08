c863b84a05de770b7446c205e411e17106be0965
Peter DePasquale <peter.depasquale@gmail.com>
2025-11-08 13:25:14 -0500

    Add example for counter of DB records in recipe model. Note tha the new method (count) in the recipe controller has support for returning the results as a HTML response, plain text, and also JSON. Additionally, the test route helpers were corrected.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get "recipes/new"
-  get "recipes/show"
-  get "recipes/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -15,5 +12,13 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "welcome#index"
 
-  resources :recipes
+  resources :recipes do
+    # Ensure the "count" route is set up first
+    collection do
+      get "count" # By using collection, Rails knows /recipes/count is an action
+      # on the collection of recipes, not a request for a recipe with the ID
+      # "count," and thus handles it correctly before moving on to the
+      # dynamic :id routes.
+    end
+  end
 end
